787d2c801ab621640a91423d8e1b25381d90f5b7
Kranthi Putti <120769579+kranthi619@users.noreply.github.com>
2023-03-28 07:55:56 +0530

    Add files via upload

---
+++ springboot-demo/src/main/java/com/star/agile/assignment/springbootdemo/HelloWorldspringbootapp.java
@@ -0,0 +1,13 @@
+package com.star.agile.assignment.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldspringbootapp {
+
+	
+public static void main(String[] args) {
+	SpringApplication.run(HelloWorldspringbootapp.class, args) ;
+}
+}
